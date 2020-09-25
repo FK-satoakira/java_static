ccb2268835feabeb21cf144b509d5397e977a85b
FK-satoakira <69459763+FK-satoakira@users.noreply.github.com>
2020-09-26 04:58:44 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,63 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Outer {
-	int outerField; static int outerStaticField;
-	static class Inner {
-		void innerMethod() {
-			outerStaticField += 10;
-			om2(); /*om()が非staticでもom内に非staticフィールドがあると不可*/
+	int outmember;
+//	List<Object> list1 = new ArrayList<>();
+
+	String outMethod(int s) {
+		int a = 10; /*非finalなのにアクセス出来るのはなぜか？*/
+		final int b = 10;
+		final int rsult = 10;
+//		final int rsult2 = 1;
+
+		class Inner{
+			private String name ;
+//			int s = s; /*フィールド定義前には参照不可*/
+			public Inner(String s) {
+				this.name=s;
+			}
+//			public void innerMethod() {
+//				System.out.println("innerMethodで計算をする");
+//				Random r = new Random();
+//				int n =2;
+//				int rr = r.nextInt(10);
+//				System.out.println(a*b*rr*rsult+n);
+////				rsult2 = (a * b * rr * rsult); /*この記述は無理。finalのrsultを再定義してるから(基本的なこと)*/
+//
+//			}
+			public String getName() {
+				return name;
+			}
+			public void setName(String name) {
+				this.name = name;
+			}
 
 		}
-	}
-	void outerMethod() {
-		Inner ic = new Inner();
-	}
-	static void om2() {
-		System.out.println("hello");
+//		List<Inner> list2 = new ArrayList<>();
+		Inner i = new Inner("www");
+		System.out.println(i.name);
+		return i.name;
+//		i.innerMethod();
+//		list2.add(i);
+//		list1.add(list2.get(0));
+//		System.out.println(list1.get(0));
 	}
 }
 
 class Main {
 	public static void main(String[] args) {
-		Outer.Inner ic = new Outer.Inner();
+		List<String> list = new ArrayList<>();
+
 		Outer o = new Outer();
-//		Outer.Inner oi = o.new Inner();
+		o.outMethod(2);
+		list.add(o.outMethod(2));
+		System.out.println(list.get(0)+"aaa");
+//		こんなんもできた。
+//		ローカルクラスを囲むメソッド内でそのローカルクラスインスタンスを生成し、外クラス(Main)で
+//		Listに入れるのも可能。
 
-		for (int i = 0; i < 11; i++) {
-			ic.innerMethod();
-			o.outerField+=1;
-
-		}
-		System.out.println(o.outerField+""+o.outerStaticField);
-				/* 無関係なクラスからはOuter.Innerで利用 */
 	}
 }
